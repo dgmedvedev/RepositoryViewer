c1bb95898efb1a36114bfdfd5567613d3db14509
dgmedvedev <dg-medvedev@mail.ru>
2023-12-20 16:08:37 +0300

    The fragment_auth.xml has been changed

--- app/src/main/java/com/demo/repositoriesviewer/presentation/AuthFragment.kt
+++ app/src/main/java/com/demo/repositoriesviewer/presentation/AuthFragment.kt
@@ -3,14 +3,10 @@ package com.demo.repositoriesviewer.presentation
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.demo.repositoriesviewer.R
@@ -46,7 +42,6 @@ class AuthFragment : Fragment() {
         binding.etAuthorization.setText(token)
 
         observeViewModel()
-
         setListeners()
 
 //        lifecycleScope.launch {
