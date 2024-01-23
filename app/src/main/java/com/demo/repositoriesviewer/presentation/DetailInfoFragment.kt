2e6f8d75a99061161e6d74e50fa0c83ad15b78b2
dgmedvedev <dg-medvedev@mail.ru>
2024-01-23 19:31:01 +0300

    DetailInfoFragment.kt, RepositoryInfoViewModel.kt have been changed

--- app/src/main/java/com/demo/repositoriesviewer/presentation/DetailInfoFragment.kt
+++ app/src/main/java/com/demo/repositoriesviewer/presentation/DetailInfoFragment.kt
@@ -5,28 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.demo.repositoriesviewer.R
-import com.demo.repositoriesviewer.databinding.FragmentAuthBinding
-import kotlinx.coroutines.launch
+import com.demo.repositoriesviewer.databinding.FragmentDetailInfoBinding
 
 class DetailInfoFragment : Fragment() {
 
-    private var _binding: FragmentAuthBinding? = null
-    private val binding: FragmentAuthBinding
-        get() = _binding ?: throw java.lang.RuntimeException("FragmentAuthBinding == null")
+    private var _binding: FragmentDetailInfoBinding? = null
+    private val binding: FragmentDetailInfoBinding
+        get() = _binding ?: throw java.lang.RuntimeException("FragmentDetailInfoBinding == null")
 
-    private lateinit var authViewModel: AuthViewModel
+    private lateinit var repositoryInfoViewModel: RepositoryInfoViewModel
 
+    private var repoId: String? = null
     private var toastMessage: Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+        repositoryInfoViewModel = ViewModelProvider(this)[repositoryInfoViewModel::class.java]
+        parseParams()
     }
 
     override fun onCreateView(
@@ -34,14 +32,12 @@ class DetailInfoFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailInfoBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val token = authViewModel.getToken()
-        binding.etAuthorization.setText(token)
 
         observeViewModel()
         setListeners()
@@ -52,67 +48,16 @@ class DetailInfoFragment : Fragment() {
         _binding = null
     }
 
-    private fun handleAction(action: AuthViewModel.Action) {
-        when (action) {
-            AuthViewModel.Action.RouteToMain -> routeSuccess()
-            is AuthViewModel.Action.ShowError -> showError(action.message)
-        }
-    }
-
-    private fun launchFragment(fragment: Fragment) {
-        requireActivity().supportFragmentManager.popBackStack()
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.container, fragment)
-            .addToBackStack(null)
-            .commit()
+    private fun parseParams() {
+        repoId = requireArguments().getString(REPO_ID)
     }
 
     private fun observeViewModel() {
-        lifecycleScope.launch {
-            authViewModel.actions.collect { action ->
-                handleAction(action)
-            }
-        }
 
-        authViewModel.state.observe(viewLifecycleOwner) { state ->
-            val color = if (state is AuthViewModel.State.Loading) {
-                ContextCompat.getColor(requireContext(), R.color.button_auth)
-            } else {
-                ContextCompat.getColor(requireContext(), R.color.white)
-            }
-            binding.signButton.setTextColor(color)
-            binding.signButton.isEnabled = state != AuthViewModel.State.Loading
-
-            binding.progressBar.visibility =
-                if (state == AuthViewModel.State.Loading) View.VISIBLE else View.GONE
-
-            binding.tilAuthorization.error = if (state is AuthViewModel.State.InvalidInput) {
-                state.reason
-            } else {
-                null
-            }
-        }
-
-        authViewModel.token.observe(viewLifecycleOwner) {
-            //TODO()
-        }
-    }
-
-    private fun routeSuccess() {
-        launchFragment(RepositoriesListFragment.getInstance())
     }
 
     private fun setListeners() {
-        with(binding) {
-            signButton.setOnClickListener {
-                val newToken: String = etAuthorization.text.toString()
-                authViewModel.repository.keyValueStorage.authToken = newToken
-                authViewModel.onSignButtonPressed()
-            }
-            etAuthorization.addTextChangedListener {
-                tilAuthorization.error = null
-            }
-        }
+
     }
 
     private fun showError(error: String) {
@@ -135,9 +80,18 @@ class DetailInfoFragment : Fragment() {
     }
 
     companion object {
-        const val HTTP_401_ERROR = "HTTP 401 "
-        const val HTTP_403_ERROR = "HTTP 403 "
-        const val HTTP_404_ERROR = "HTTP 404 "
-        const val HTTP_422_ERROR = "HTTP 422 "
+        private const val HTTP_401_ERROR = "HTTP 401 "
+        private const val HTTP_403_ERROR = "HTTP 403 "
+        private const val HTTP_404_ERROR = "HTTP 404 "
+        private const val HTTP_422_ERROR = "HTTP 422 "
+        private const val REPO_ID = "REPO_ID"
+
+        fun getInstance(repoId: String): DetailInfoFragment {
+            return DetailInfoFragment().apply {
+                arguments = Bundle().apply {
+                    putString(REPO_ID, repoId)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/demo/repositoriesviewer/presentation/RepositoryInfoViewModel.kt
+++ app/src/main/java/com/demo/repositoriesviewer/presentation/RepositoryInfoViewModel.kt
@@ -3,19 +3,61 @@ package com.demo.repositoriesviewer.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.demo.repositoriesviewer.data.AppRepositoryImpl
 import com.demo.repositoriesviewer.domain.entities.Repo
+import com.demo.repositoriesviewer.domain.entities.RepoDetails
+import com.demo.repositoriesviewer.domain.usecases.GetRepositoryReadmeUseCase
 import com.demo.repositoriesviewer.domain.usecases.GetRepositoryUseCase
+import kotlinx.coroutines.launch
 
 class RepositoryInfoViewModel : ViewModel() {
 
     val repository: AppRepositoryImpl = AppRepositoryImpl
 
     private val getRepositoryUseCase = GetRepositoryUseCase(repository)
+    private val getRepositoryReadmeUseCase = GetRepositoryReadmeUseCase(repository)
 
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
         get() = _state
+
+    fun loadData(repoId: String) {
+        viewModelScope.launch {
+            val readme: String
+            val repo: Repo
+            val repositoryDetails: RepoDetails
+            try {
+                _state.value = State.Loading
+                repositoryDetails = getRepositoryUseCase(repoId)
+                repo = Repo(repoId, repositoryDetails)
+                _state.value = State.Loaded(repo, ReadmeState.Loading)
+                val ownerName = repo.repoDetails.userInfo?.name
+                val repositoryName = repo.repoDetails.name
+                val branchName = repo.repoDetails.branchName
+
+                if (!ownerName.isNullOrBlank()) {
+                    readme = getRepositoryReadmeUseCase(ownerName, repositoryName, branchName)
+                    _state.value = State.Loaded(repo, ReadmeState.Loaded(readme))
+                    if (ownerName.isEmpty() && repositoryName.isEmpty() && branchName.isEmpty()) {
+                        _state.value = State.Loaded(repo, ReadmeState.Empty)
+                    }
+                } else {
+                    _state.value =
+                        State.Loaded(repo, ReadmeState.Error("ownerName.isNullOrBlank()"))
+                }
+            } catch (error: Throwable) {
+                showError(error)
+            }
+        }
+    }
+
+    private fun showError(error: Throwable) {
+        when (error) {
+            is Exception -> _state.value = State.Error(error.message.toString())
+            is Error -> throw Error(error.message)
+        }
+    }
 
     sealed interface State {
         object Loading : State
