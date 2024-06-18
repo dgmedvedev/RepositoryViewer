20ac0157613e26aef48771cc696afddfb1fa1093
dgmedvedev <dg-medvedev@mail.ru>
2024-06-18 17:32:32 +0300

    changed the ApiFactory

--- data/src/main/java/com/demo/repositoriesviewer/data/network/ApiFactory.kt
+++ data/src/main/java/com/demo/repositoriesviewer/data/network/ApiFactory.kt
@@ -5,25 +5,27 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiFactory private constructor() {
 
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
+
     companion object {
 
         private const val BASE_URL = "https://api.github.com/"
 
-        private val retrofit = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(BASE_URL)
-            .build()
-
         @Volatile
-        private var instanceApiService: ApiService? = null
+        private var instance: ApiFactory? = null
 
-        fun getInstanceApiService(): ApiService {
-            instanceApiService?.let { return it }
+        fun getInstance(): ApiFactory {
+            instance?.let { return it }
             synchronized(ApiFactory::class.java) {
-                instanceApiService?.let { return it }
-                val apiService: ApiService = retrofit.create(ApiService::class.java)
-                instanceApiService = apiService
-                return apiService
+                instance?.let { return it }
+                val apiFactory = ApiFactory()
+                instance = apiFactory
+                return apiFactory
             }
         }
     }
